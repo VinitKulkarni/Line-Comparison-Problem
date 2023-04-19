407084115e08c585f18e95efad67c25f25c9b5a9
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2023-04-19 13:57:19 +0530

    Delete EmployeeWageCalculation.java
    my mistake file uploaded

--- EmployeeWageCalculation.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.Day3PP;
-
-public class EmployeeWageCalculation {
-    public static void main(String[] args) {
-        System.out.println("Employee Wage Calculation");
-    }
-}
